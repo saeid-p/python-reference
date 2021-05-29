55557964778d5a0a658960ae594210a7bca6f252
sayid-coder <pournejati@gmail.com>
2021-05-29 14:36:11 -0500

    Add an introto the execution of python commands.

---
+++ intro.py
@@ -0,0 +1,15 @@
+# Run this file by executing $ python intro.py
+
+print("Hello, World!")
+
+# By executing the "python" command in your terminal, you are presented with an interactive Python shell.
+# This is also known as the Python Interpreter or a REPL (for 'Read Evaluate Print Loop')
+
+# You can run command as strings.
+# $ python -c 'print("Hello, World")'
+
+# Package Management:
+# The PyPA recommended tool for installing Python packages is PIP.
+# To install, on your command line execute $ pip install <package name>
+# Note: On windows you must add pip to your PATH environment variables.
+    # To avoid this, use $ python -m pip install <package name>
