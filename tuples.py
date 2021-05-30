5b3db295ea28d6e863763f72a112a39f7e0f1f59
Saeid Pournejati <pournejati@gmail.com>
2021-05-30 00:11:15 -0500

    Add more examples to tuples.

--- tuples.py
+++ tuples.py
@@ -1,6 +1,15 @@
 # Tuples are ordered collection of n values of any type.
 # Tuple supports indexing. It is immutable and hashable if all its members are hashable.
+# Tuples are commonly used for small collections of values that will not need to change.
 
-t1 = (1, 2, 3)
-t2 = ('a', 1, 'Test Phrase', (1, 2))
-print(t2[2], t2[3])
+# Tuple classic syntax:
+t0 = tuple(['Only member'])
+
+t1 = (1,2,3)
+t2 = ('a', 1, 'Test Phrase', (1,2), t1)
+
+print(t2[2]) # 'Test Phrase'
+print(t2[4]) # (1,2,3)
+
+t3 = (1, 2)
+print(t3 == t2[3]) # True
