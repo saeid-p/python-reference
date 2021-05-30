f288e3e57a745522a2889595ee03c4235ec8bb24
sayid-coder <pournejati@gmail.com>
2021-05-29 22:07:04 -0500

    Add tupple examples.

---
+++ tuples.py
@@ -0,0 +1,6 @@
+# Tuples are ordered collection of n values of any type.
+# Tuple supports indexing. It is immutable and hashable if all its members are hashable.
+
+t1 = (1, 2, 3)
+t2 = ('a', 1, 'Test Phrase', (1, 2))
+print(t2[2], t2[3])
