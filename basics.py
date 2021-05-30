22730892ccee49bb334ed74a4b3be53786d2da49
sayid-coder <pournejati@gmail.com>
2021-05-29 20:50:52 -0500

    Add reference type examples.

--- basics.py
+++ basics.py
@@ -1,11 +1,9 @@
 # Variable names are case sensitive and can start with _
 _variable1 = 'My Special Variable.'
-print(_variable1)
 
-# You can have multilple assignments. If the count of two sides don't match, and error will be thrown.
-a, b, c = 1, 2, 3
-
-x = y = z = 1
+# Get type of variable with type()
+variableType = type(_variable1)
+print(variableType)
 
 num1 = 5 # typeof(num1) = <type 'int'>
 num2 = 3.14 # typeof(num2) = <type 'float'>
@@ -16,9 +14,18 @@ str2 = "A"
 bool1 = True # typeof(bool1) = <type 'bool'>
 bool2 = False
 
-# Get type of varilable with type()
-bool2Type = type(bool2)
-print(bool2Type)
-
 nullValue = None # Null values are defined as 'None'
 print(nullValue) # Output: None, typeof(nullValue) = <class 'NoneType'>
+
+# You can have multiple assignments. If the count of two sides don't match, and error will be thrown.
+a, b, c = 1,2,3
+x = y = z = 1
+
+# Reference types like lists and dictionaries are acting accordingly.
+l1 = l2 = [1,2,3]
+l1[0] = 5
+print(l2[0]) # 5
+
+l3 = l4 = [1,2,3]
+l3 = [4,5,6]
+print(l4[0]) # 1
