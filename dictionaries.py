707f87d11c64214ba63535966be1147097180260
sayid-coder <pournejati@gmail.com>
2021-05-29 22:07:14 -0500

    Add dictionaries example.

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# Dictionaries are unordered collection of unique key-value pairs; keys must be hashable.
+d1 = {
+    1: 'one',
+    2: 'two'
+ }
+
+d2 = {
+    'a': [1, 2, 3],
+    'b': 'a string'
+}
+
+print(d1[1])
+print(d2['a'])
