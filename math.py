d20efbe16852d375c9561f608979d44e3bc6166f
Saeid Pournejati <pournejati@gmail.com>
2021-05-30 15:04:39 -0500

    Include log examples.

--- math.py
+++ math.py
@@ -1,11 +1,21 @@
 pow(2,3) # 8
 
+3 % 4 # 3
+10 % 2 # 0
+6 % 4 # 2
+
 import math
+import cmath
 
 math.pow(2,3) # 8
 math.sqrt(16) # 4.0
 math.floor(15.7) # 15.0
 math.ceil(13.2) # 14.0
-math.log(17)
-math.log10(17)
 math.pi # 3.141592653589793
+
+math.log(5) # = 1.6094379124341003
+# optional base argument. Default is math.e
+math.log(5, math.e) # = 1.6094379124341003
+cmath.log(5) # = (1.6094379124341003+0j)
+math.log(1000, 10) # 3.0 (always returns float)
+cmath.log(1000, 10) # (3+0j)
