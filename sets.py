cad494f20457f1d3ed7b5fc3a4a63a7e333f18af
Saeid Pournejati <pournejati@gmail.com>
2021-05-30 12:26:07 -0500

    Add iteration example.

--- sets.py
+++ sets.py
@@ -1,2 +1,22 @@
 # Sets are unordered collection of unique values. Items must be hashable.
-s1 = {1, 2, 'a'}
+
+s1 = {1,2,'a'}
+
+s1.add(3)
+s1.remove('a')
+
+print(s1) # {1,2,3}
+
+print(s1.pop()) # Remove an arbitrary element from the set.
+
+
+l1 = [1,2,3]
+s2 = set(l1)
+print(s2) # {1,2,3}
+
+if 1 in s2:
+    print('Found!')
+    
+# You can iterate like a list, but the values will be in an arbitrary order.
+for item in s2:
+    print(item)
