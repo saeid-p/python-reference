382481839435f6b3d36d6d48c3747a5fa3bef66c
saeid-p <pournejati@gmail.com>
2025-01-20 13:01:48 -0800

    Updated array definitions.

--- arrays.py
+++ arrays.py
@@ -1,40 +1,29 @@
-from array import *
-
-"""
-In the array initializer, "Type code" represents signed integer of size 1 byte:
-    B Represents unsigned integer of size 1 byte
-    c Represents character of size 1 byte
-    u Represents unicode character of size 2 bytes
-    h Represents signed integer of size 2 bytes
-    H Represents unsigned integer of size 2 bytes
-    i Represents signed integer of size 2 bytes
-    I Represents unsigned integer of size 2 bytes
-    w Represents unicode character of size 4 bytes
-    l Represents signed integer of size 4 bytes
-    L Represents unsigned integer of size 4 bytes
-    f Represents floating point of size 4 bytes
-    d Represents floating point of size 8 bytes
-"""
-
-a1 = array('i', [1,2,3,4])
+a1 = [1, 2, 3, 3, 4]
 
 for item in a1:
     print(item)
-    
-a1.append(5)
-a1.insert(0,0)
 
-a2 = array('i', [6,7,8])
-a1.extend(a1)
+# Return the number of times and element appears in an array:
+occurrence = a1.count(3)  # 2
 
-targetIndex = a2.index(6) # Find element's index.
+a1.append(5)  # Add element to end of array.
+a1.insert(0, 10)  # Insert element 10 at index 0.
 
-a2.remove(8) # Remove by instance
-a2.pop(len(a2) - 1) # Removes by index
-a2.pop() # Remove last element.
+a2 = [6, 7, 8]
+a1.extend(a2)  # Add elements of a2 to end of a1.
+
+targetIndex = a2.index(6)  # Find element's index.
+a2.pop(targetIndex)  # Removes by index
+a2.remove(8)  # Remove by instance
+a2.pop()  # Remove last element.
 
 a2.reverse()
 
-# Return the number of times and element appears in an array:
-a3 = [1,2,3,3,3,3]
-print(a3.count(3)) # 4
+a2.sort()  # Sort in ascending order (Inplace)
+a2.sort(reverse=True)  # Sort in descending order (Inplace)
+
+# Sort a2 in ascending order using lambda
+a2.sort(key=lambda x: x)
+
+# Sort a2 in descending order using lambda
+a2.sort(key=lambda x: -x)
