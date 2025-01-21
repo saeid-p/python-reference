b73c00418d7eff905a3fda80985fa0d2200421ec
saeid-p <pournejati@gmail.com>
2025-01-20 22:31:17 -0800

    Update class notes.

--- class-examples.py
+++ class-examples.py
@@ -1,44 +1,48 @@
+# Python has no real concept of private elements.
+
+
 class MyClass(object):
     """This is a test class."""
-    # Python has no real concept of private elements.
-    
-    """Default class initializer"""
+
     # Every method, included in the class definition passes the object in question as its first parameter.
     # The word 'self' is used for this parameter (usage of self is actually by convention).
-    def __init__(self, prop1 = None):
+    def __init__(self, prop1=None):
+        """Default class initializer"""
         self.prop1 = prop1
-    
+
     def getProp1(self):
         return self.prop1
-    
+
     def setProp1(self, value):
         self.prop1 = value
-    
+
     @property
     def prop2(self):
         """A profoundly important string."""
         return self._prop2
-    
+
     @prop2.setter
-    def prop2(self, value):        
+    def prop2(self, value):
         self._prop2 = value
-    
+
     def myFunc(self, param1):
         """This is a test function."""
         print(param1)
 
+
 class MyDerivedClass(MyClass):
     def extraMethod():
-        print('Child Class!')
+        print("Child Class!")
+
 
 instance1 = MyClass()
-instance1.myFunc('Message')
+instance1.myFunc("Message")
 print(instance1.getProp1())
 instance1.prop2 = "String!"
 
 instance2 = MyDerivedClass("First Value")
 print(instance2.getProp1())
-instance2.setProp1('New Value')
+instance2.setProp1("New Value")
 print(instance2.getProp1())
 
 # Python uses the C3 linearization algorithm to determine the order in which to resolve class attributes,
