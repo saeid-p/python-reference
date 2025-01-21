2ded055b63cd90cca21949bee89f9d88898a5a07
saeid-p <pournejati@gmail.com>
2025-01-20 23:42:45 -0800

    Update dictionary notes.

--- dictionaries.py
+++ dictionaries.py
@@ -1,54 +1,46 @@
+from collections import defaultdict, OrderedDict
+
 # Dictionaries are unordered collection of unique key-value pairs; keys must be hashable.
-d1 = {
-    1: 'one',
-    2: 'two'
- }
+d1 = {1: "one", 2: "two"}
 
-print(d1[1]) # one
+print(d1[1])  # one
 
-d1[3] = 'three'
-print(d1[3]) # three
+d1[3] = "three"
+print(d1[3])  # three
 
 d1.pop(3)
-print(d1) # {1: 'one', 2: 'two'}
+print(d1)  # {1: 'one', 2: 'two'}
 
-d2 = {
-    'a': [1, 2, 3],
-    'b': 'String Value'
-}
+d2 = {"a": [1, 2, 3], "b": "String Value"}
 
 for key in d2.keys():
-    print('Key: \'{}\' - Value: \'{}\''.format(key, d2[key]))
+    print("Key: '{}' - Value: '{}'".format(key, d2[key]))
 
 for key, value in d2.items():
     print(key, "::", value)
 
 #### Safe Search ####
-item  = d2.get('c', 'default')
+item = d2.get("c", "default")
 
-if 'd' in d2:
-    value = d2['d']
+if "d" in d2:
+    value = d2["d"]
 
-#### defaultdict ####
 # A defaultdict is a dictionary with a default value for keys.
 # Keys for which no value has been explicitly defined can be accessed without errors.
-from collections import defaultdict
-
 dd1 = defaultdict(lambda: -1)
 dd1[1] = 5
 
-print('dd1[1]', dd1[1]) # 5
-print('dd1[2]', dd1[2]) # -1
+print("dd1[1]", dd1[1])  # 5
+print("dd1[2]", dd1[2])  # -1
 
-#### Ordered Dictionary ####
-from collections import OrderedDict
+# Ordered Dictionary maintains the order in which the keys are inserted, unlike a regular dictionary which does not guarantee any order.
 od1 = OrderedDict()
-od1['first'] = 1
-od1['second'] = 2
-od1['third'] = 3
-od1['last'] = 4
+od1["first"] = 1
+od1["second"] = 2
+od1["third"] = 3
+od1["last"] = 4
 
 #### Dictionary Comprehensions ####
 # Dictionary comprehension creates a new dictionary by applying an expression to each element of an iterable.
 dc1 = {x: x * x for x in (1, 2, 3, 4)}
-dc2 = {name: len(name) for name in ('Stack', 'Overflow', 'Exchange') if len(name) > 6}
+dc2 = {name: len(name) for name in ("Stack", "Overflow", "Exchange") if len(name) > 6}
